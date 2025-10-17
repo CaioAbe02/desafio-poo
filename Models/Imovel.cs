4631a0ff8968b47ea765b1b54b11177f9e676ffc
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-10-17 10:17:35 -0300

    feat: Imovel model

---
+++ Models/Imovel.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DesafioPOO.Models;
+
+public abstract class Imovel
+{
+  [Key]
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+  public int Id { get; set; }
+
+  [Required]
+  [StringLength(150)]
+  protected string Endereco { get; set; } = default!;
+
+  [Required]
+  protected int Numero { get; set; } = default!;
+
+  [Required]
+  protected bool Alugado { get; set; } = default!;
+
+  [ForeignKey(nameof(Proprietario))]
+  public int ProprietarioId { get; set; }
+
+  [Required]
+  public Proprietario Proprietario { get; set; } = default!;
+
+  public abstract string EstaAlugado();
+  public string ContatoProprietario() => Proprietario.Contato();
+  public int CalcularAluguel(int valorBase) => valorBase;
+
+  public string ObterEndereco() => Endereco;
+  public int ObterNumero() => Numero;
+  public bool ObterStatusAluguel() => Alugado;
+
+  public void AlterarEndereco(string endereco) => Endereco = endereco;
+  public void AlterarNumero(int numero) => Numero = numero;
+  public void AlterarStatusAluguel(bool status) => Alugado = status;
+}
