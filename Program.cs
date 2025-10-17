e5cc216564f51101b7014418cb08a81afb268529
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-10-17 13:23:58 -0300

    refactor: move proprietario endpoints to another file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 using DesafioPOO.Db;
 using DesafioPOO.Interfaces;
-using DesafioPOO.DTOs;
 using Microsoft.EntityFrameworkCore;
-using DesafioPOO.Models;
-using Microsoft.AspNetCore.Mvc;
 using DesafioPOO.Services;
+using DesafioPOO.Endpoints;
 
 #region Builder
 var builder = WebApplication.CreateBuilder(args);
@@ -25,50 +23,8 @@ builder.Services.AddDbContext<DbContexto>(options =>
 var app = builder.Build();
 #endregion
 
-#region Proprietario
-app.MapGet("/proprietarios", (IProprietarioService proprietatioService) =>
-{
-  var proprietarios = proprietatioService.Todos();
-
-  return Results.Ok(proprietarios);
-}).WithTags("Proprietario");
-
-app.MapGet("proprietarios/{id}", ([FromRoute] int id, IProprietarioService proprietarioService) =>
-{
-  var proprietario = proprietarioService.BuscaPorId(id);
-  if (proprietario == null)
-    return Results.NotFound();
-  return Results.Ok(proprietario);
-}).WithTags("Proprietario");
-
-app.MapPost("/proprietarios", ([FromBody] ProprietarioDTO proprietarioDTO, IProprietarioService proprietatioService) =>
-{
-  var proprietario = new Proprietario
-  {
-    Nome = proprietarioDTO.Nome,
-    Telefone = proprietarioDTO.Telefone,
-    Cpf = proprietarioDTO.Cpf
-  };
-
-  proprietatioService.Adicionar(proprietario);
-
-  return Results.Created($"/proprietarios/{proprietario.Id}", proprietario);
-}).WithTags("Proprietario");
-
-app.MapPut("/proprietarios/{id}", ([FromRoute] int id, ProprietarioDTO proprietarioDTO, IProprietarioService proprietatioService) =>
-{
-  var proprietario = proprietatioService.BuscaPorId(id);
-  if (proprietario == null)
-    return Results.NotFound();
-
-  proprietario.Nome = proprietarioDTO.Nome;
-  proprietario.Telefone = proprietarioDTO.Telefone;
-  proprietario.Cpf = proprietarioDTO.Cpf;
-
-  proprietatioService.Atualizar(proprietario);
-
-  return Results.Ok(proprietario);
-}).RequireAuthorization().WithTags("Proprietario");
+#region Endpoints
+app.MapProprietarioEndpoints();
 #endregion
 
 #region App
