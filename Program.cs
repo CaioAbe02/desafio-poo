ff7ce9263c8da361b360ae93cdea019b6fc9f669
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-10-17 11:54:49 -0300

    feat: Proprietario get and post routes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 using DesafioPOO.Db;
+using DesafioPOO.Interfaces;
+using DesafioPOO.DTOs;
 using Microsoft.EntityFrameworkCore;
+using DesafioPOO.Models;
+using Microsoft.AspNetCore.Mvc;
+using DesafioPOO.Services;
 
 #region Builder
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IProprietarioService, ProprietarioService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +23,29 @@ builder.Services.AddDbContext<DbContexto>(options =>
 });
 
 var app = builder.Build();
+#endregion
+
+#region Proprietario
+app.MapGet("/proprietarios", (IProprietarioService proprietatioService) =>
+{
+  var proprietarios = proprietatioService.Todos();
+
+  return Results.Ok(proprietarios);
+}).WithTags("Proprietario");
+
+app.MapPost("/proprietarios", ([FromBody] ProprietarioDTO proprietarioDTO, IProprietarioService proprietatioService) =>
+{
+  var proprietario = new Proprietario
+  {
+    Nome = proprietarioDTO.Nome,
+    Telefone = proprietarioDTO.Telefone,
+    Cpf = proprietarioDTO.Cpf
+  };
+
+  proprietatioService.Adicionar(proprietario);
+
+  return Results.Created($"/proprietarios/{proprietario.Id}", proprietario);
+}).WithTags("Proprietario");
 #endregion
 
 #region App
