e6b259798883416341ad466d2e5aa25014e34085
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-10-17 15:20:23 -0300

    feat: migration

---
+++ Migrations/20251017181348_AddValorAluguelField.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioPOO.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValorAluguelField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ValorAluguel",
+                table: "Casas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ValorAluguel",
+                table: "Apartamentos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorAluguel",
+                table: "Casas");
+
+            migrationBuilder.DropColumn(
+                name: "ValorAluguel",
+                table: "Apartamentos");
+        }
+    }
+}
